8778f0c46d280a94f13b92ebff06fc7a3eb7f39e
Lasse Tvedegaard <lassetvedegaard@gmail.com>
2024-07-29 23:26:17 +0200

    Better icons

--- src/components/Menu/MenuItems.js
+++ src/components/Menu/MenuItems.js
@@ -1,12 +1,11 @@
-import { AiFillPieChart } from 'react-icons/ai';
-import { SiFuturelearn } from 'react-icons/si';
+import { AiFillPieChart, AiOutlineRead, AiOutlineBook, AiOutlineClockCircle, AiOutlineCheckCircle } from 'react-icons/ai';
 
 const MenuItems = [
   { title: 'Dashboard', path: '/dashboard', icon: <AiFillPieChart /> },
-  { title: 'Books', path: '/books', icon: <SiFuturelearn /> },
-  { title: 'Currently Reading', path: '/currentlyReading', icon: <SiFuturelearn /> },
-  { title: 'Books read', path: '/read', icon: <SiFuturelearn /> },
-  { title: 'Books to read', path: '/willRead', icon: <SiFuturelearn /> },
+  { title: 'Books', path: '/books', icon: <AiOutlineBook /> },
+  { title: 'Currently Reading', path: '/currentlyReading', icon: <AiOutlineClockCircle /> },
+  { title: 'Books read', path: '/read', icon: <AiOutlineCheckCircle /> },
+  { title: 'Books to read', path: '/willRead', icon: <AiOutlineRead /> },
 ];
 
 export default MenuItems;
