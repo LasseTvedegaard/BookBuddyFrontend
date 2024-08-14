8861d1653f5dff94bad8fe2a56f0a603ab745174
Lasse Tvedegaard <lassetvedegaard@gmail.com>
2024-08-14 14:45:04 +0200

    Background scrolling issue now works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  // enable dark mode via class strategy
-  darkMode: "class",
-
+  darkMode: "class", // enable dark mode via class strategy
   theme: {
     extend: {
       colors: {
